afe566206fa1ce4f99dca3c7e8a5faf38c732af3
Peter Fisk <peter.fisk@gmail.com>
2026-02-07 12:57:00 -0500

    updates

---
+++ BlazorLib/Constants/AppContants.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorLib.Constants
+{
+    public class AppContants
+    {
+        public const string AppName = "Cobol AI Studio";
+        public const string LastUpdated = "2026-02-06T21:29:24Z";
+        public const string TOKEN = "pfGHuyuVLaC0qVxFkJ7yW0OzMhmHJFkx2XLTdx9C";
+    }
+}
