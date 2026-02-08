527a7d098919741505b9877d41d4f2c2509b2bf5
Peter Fisk <peter.fisk@gmail.com>
2026-02-08 09:00:11 -0500

    refresh

--- BlazorLib/deploy.ps1
+++ BlazorLib/deploy.ps1
@@ -1,4 +1,3 @@
 ﻿cd bin/Release/net10.0/browser-wasm/publish
 npx wrangler pages deploy ./wwwroot --project-name cobol-ai-studio-site
 cd ../../../../..
-
--- CobolAiStudioUI/Constants/AppContants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CobolAiStudioUI.Constants
-{
-    public class AppContants
-    {
-        public const string AppName = "Cobol AI Studio";
-        public const string LastUpdated = "2026-02-06T17:56:38Z";
-        public const string TOKEN = "pfGHuyuVLaC0qVxFkJ7yW0OzMhmHJFkx2XLTdx9C";
-    }
-}
